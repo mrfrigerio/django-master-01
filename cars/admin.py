e5010fc6d91297c57bb5a94f6eab6e7a620ff684
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2023-09-17 00:43:22 -0300

    Criada a primeira view (cars.html)

--- cars/admin.py
+++ cars/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 
 # Register your models here.
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ("model", "brand", "factory_year", "model_year", "value")
-    search_fields = ("model",)
+    search_fields = ("model", "brand")
 
 
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def cars_view(request):
+    return render(
+        request, template_name="cars.html", context={"cars": {"model": "Astra 2.0"}}
+    )
