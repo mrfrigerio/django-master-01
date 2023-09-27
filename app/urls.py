0078ff924a49ef94be2055ee5c21d6c035d51ab2
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2023-09-26 22:51:05 -0300

    Adicionada CBVs na view de carros

--- app/urls.py
+++ app/urls.py
@@ -7,10 +7,11 @@ import accounts.views
 import cars.views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("cars/", cars.views.cars_view, name="cars_list"),
-    path("new_car/", cars.views.new_car_view, name="new_car"),
-    path("register/", accounts.views.register_view, name="register"),
-    path("login/", accounts.views.login_view, name="login"),
-    path("logout/", accounts.views.logout_view, name="logout")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path("admin/", admin.site.urls),
+                  path("cars/", cars.views.CarsListView.as_view(), name="cars_list"),
+                  path("new_car/", cars.views.NewCarCreateView.as_view(), name="new_car"),
+                  path("car/<int:pk>/", cars.views.CarDetailView.as_view(), name="car_detail"),
+                  path("register/", accounts.views.register_view, name="register"),
+                  path("login/", accounts.views.login_view, name="login"),
+                  path("logout/", accounts.views.logout_view, name="logout")
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,31 +1,84 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, CreateView, DetailView
 
 from cars.forms import CarModelForm
 from cars.models import Car
 
 
+# CLASS BASED VIEWS
+# class CarsView(View):
+#     def get(self, request):
+#         query = request.GET
+#         cars = []
+#         if query.get("search"):
+#             cars = Car.objects.filter(model__icontains=query.get("search")).order_by(
+#                 "model"
+#             )
+#         else:
+#             cars = Car.objects.all().order_by("-model")
+#         return render(request, template_name="cars.html", context={"cars": cars})
+
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get("search")
+
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
+
+
+# class NewCarView(View):
+#     @staticmethod
+#     def get(request):
+#         new_car_form = CarModelForm()
+#         return render(request, template_name="new_car.html", context={"new_car_form": new_car_form})
+#
+#     @staticmethod
+#     def post(request):
+#         new_car_form = CarModelForm(request.POST, request.FILES)
+#         if new_car_form.is_valid():
+#             new_car_form.save()
+#             return redirect('cars_list')
+#         return render(request, template_name="new_car.html", context={"new_car_form": new_car_form})
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+# FUNCTION BASED VIEWS
 # Create your views here.
-@require_http_methods(["GET"])
-def cars_view(request):
-    query = request.GET
-    cars = []
-    if query.get("search"):
-        cars = Car.objects.filter(model__icontains=query.get("search")).order_by(
-            "model"
-        )
-    else:
-        cars = Car.objects.all().order_by("-model")
-    return render(request, template_name="cars.html", context={"cars": cars})
+# @require_http_methods(["GET"])
+# def cars_view(request):
+#     query = request.GET
+#     cars = []
+#     if query.get("search"):
+#         cars = Car.objects.filter(model__icontains=query.get("search")).order_by(
+#             "model"
+#         )
+#     else:
+#         cars = Car.objects.all().order_by("-model")
+#     return render(request, template_name="cars.html", context={"cars": cars})
 
 
-def new_car_view(request):
-    if request.method == "POST":
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, template_name="new_car.html", context={"new_car_form": new_car_form})
+# def new_car_view(request):
+#     if request.method == "POST":
+#         new_car_form = CarModelForm(request.POST, request.FILES)
+#         if new_car_form.is_valid():
+#             new_car_form.save()
+#             return redirect('cars_list')
+#     else:
+#         new_car_form = CarModelForm()
+#     return render(request, template_name="new_car.html", context={"new_car_form": new_car_form})
